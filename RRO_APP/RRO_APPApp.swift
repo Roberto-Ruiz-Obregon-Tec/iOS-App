d4b2f91d05667df41b3469d5d33278bf1a20b51c
maugarciav <mau.villanueva2002@gmail.com>
2023-10-16 12:03:38 -0600

    subiendo app vacia

---
+++ RRO_APP/RRO_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  RRO_APPApp.swift
+//  RRO_APP
+//
+//  Created by Mauricio Garcia Villanueva on 16/10/23.
+//
+
+import SwiftUI
+
+@main
+struct RRO_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
