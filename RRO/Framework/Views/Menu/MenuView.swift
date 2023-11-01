f0c7e71aea1515360eab1a20cb4d2a27fd2996e0
sjimenezbauer <sjimenezbauer@gmail.com>
2023-11-01 09:26:52 -0600

    hotfix: Arreglar .pbxproj

---
+++ RRO/Framework/Views/Menu/MenuView.swift
@@ -0,0 +1,39 @@
+//
+//  MenuView.swift
+//  RRO
+//
+//  Created by peblo on 20/10/23.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    let goLogin: () -> Void
+    var body: some View {
+        TabView {
+            // TODO: wrap courses, scholarships, and programs on a single view
+            InicioView().tabItem {
+                Image(systemName: "house")
+                Text("Inicio")
+            }
+            FeedView().tabItem {
+                Image(systemName: "globe")
+                Text("Publicaciones")
+            }
+            FAQView().tabItem {
+                Image(systemName: "envelope")
+                Text("FRRO")
+            }
+            ProfileView(goLogin: goLogin).tabItem {
+                Image(systemName: "person")
+                Text("Perfil")
+            }
+        }
+    }
+}
+
+struct MenuViewPreview: PreviewProvider {
+    static var previews: some View {
+        MenuView{()}
+    }
+}
