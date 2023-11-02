69216fd37e12af736f4b35a4c62caef949511ea4
XxGonanxX <patomorelia@hotmail.com>
2023-11-02 14:03:15 -0600

    feature: appicon added

--- RRO/Data/Repositories/RROAPI.swift
+++ RRO/Data/Repositories/RROAPI.swift
@@ -11,7 +11,7 @@ struct API {
     // TODO: Change the url with the deployed domain, private ip used for testing
             //                 |
                 //PONER TU IP AQUÍ v
-    static let base = "http://10.25.107.225:3001/v1"
+    static let base = "http://169.254.179.227:3001/v1"
     
     struct routes {
         // TODO: Map all the routes
