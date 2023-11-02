be657e805fab88fbdbba016dce140db73e80683c
ArmandoRosasB <armando.rosas133@gmail.com>
2023-11-01 20:00:21 -0600

    fix:  prints

--- RRO/Data/Repositories/RROAPI.swift
+++ RRO/Data/Repositories/RROAPI.swift
@@ -11,7 +11,7 @@ struct API {
     // TODO: Change the url with the deployed domain, private ip used for testing
             //                 |
                 //PONER TU IP AQUÍ v
-    static let base = "http://192.168.68.106:3001/v1"
+    static let base = "http://localhost:3001/v1"
     
     struct routes {
         // TODO: Map all the routes
