0e2b6072f7980b1819bf709b11b0afcbea464c27
A01709150 <A01709150@tec.mx>
2023-11-08 15:11:13 -0600

    feat: view course details

--- RRO/Data/Repositories/RROAPI.swift
+++ RRO/Data/Repositories/RROAPI.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct API {
-    // TODO: Change the url with the deployed domain, private ip used for testing
+    // TODO: Change the url with tohe deployed domain, private ip used for testing
             //                 |
                 //PONER TU IP AQUÍ v
     static let base = "http://10.25.82.65:3000/v1"
