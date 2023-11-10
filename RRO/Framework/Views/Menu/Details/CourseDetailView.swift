5d7c7552727af39d559fee52fae44e79931bb67c
A01709150 <A01709150@tec.mx>
2023-11-10 12:28:36 -0600

    feat: Mejorar el filtro de texto y arreglar la vista detalles de acuerdo con lo que dijo la socia

--- RRO/Framework/Views/Menu/Details/CourseDetailView.swift
+++ RRO/Framework/Views/Menu/Details/CourseDetailView.swift
@@ -32,15 +32,30 @@ struct CourseDetailView: View {
                         
                         Spacer()
                     }
+                    HStack {
+                        Image(systemName: "person.fill")
+                        Text(course.speaker)
+                            .fontWeight(.bold)
+                        Spacer()
+                        Text(String(course.rating))
+                        Image(systemName: "star.fill")
+                    }
                     
                     VStack(spacing: 8) {
-                        Text("Fecha límite para enviar datos:")
-                        HStack {
+                        HStack{
                             Image(systemName: "calendar")
+                            Text("Fecha:")
+                        }
+                        
+                        HStack {
+                            
                             
                             Text(course.startDate!.toISODate(), format: .dateTime.day().month().year())
+                            Text("-")
+                            Text(course.endDate!.toISODate(), format: .dateTime.day().month().year())
                             
-                            Image(systemName: "calendar")
+                            
+                            
                         }
                     }
                     .padding(.vertical)
@@ -48,6 +63,7 @@ struct CourseDetailView: View {
                     .fontWeight(.bold)
                     
                     VStack(spacing: 8) {
+                        
                         HStack {
                             Text("Detalles")
                                 .fontWeight(.bold)
@@ -76,44 +92,24 @@ struct CourseDetailView: View {
                                 .foregroundStyle(.secondary)
                         }
                         
-                        Divider()
+                        
                         
                         if course.modality == "Presencial"{
+                            Divider()
                             HStack {
-                                Text("Codigo Postal")
+                                Text("Ubicación")
                                     .fontWeight(.bold)
                                 
                                 Spacer()
                                 
                                 HStack {
                                     Image(systemName: "location")
-                                    Text(String(course.postalCode!))
+                                    Text(String(course.location!))
                                 }.foregroundStyle(.secondary)
                             }
-                            
-                            Divider()
-                            
+
                         }
-                        
-                        
-                        
-                        
-                        
-                        HStack {
-                            Text("Fecha")
-                                .fontWeight(.bold)
-                            
-                            Spacer()
-                            
-                            
-                            HStack {
-                                Image(systemName: "calendar")
-                                Text(course.startDate!.toISODate(), format: .dateTime.day().month().year())
-                                Text("-")
-                                Text(course.endDate!.toISODate(), format: .dateTime.day().month().year())
-                                
-                            }.foregroundStyle(.secondary)
-                        }
+                                            
                         
                         Divider()
                         
@@ -145,12 +141,12 @@ struct CourseDetailView: View {
                         }
                         else{
                             HStack {
-                                Text("Costo (en pesos mexicanos)")
+                                Text("Costo")
                                     .fontWeight(.bold)
                                 
                                 Spacer()
                                 
-                                Text("$" + String(course.cost))
+                                Text("$" + String(course.cost) + " MXN")
                                     .foregroundStyle(.secondary)
                             }
                         }
@@ -159,13 +155,27 @@ struct CourseDetailView: View {
                         Divider()
                         
                         HStack {
-                            Text("Capacidad de personas")
+                            Text("Cupos disponibles")
                                 .fontWeight(.bold)
                             
                             Spacer()
                             
                             HStack {
                                 Text(String(course.capacity))
+                            }.foregroundStyle(.secondary)
+                            
+                        }
+                        
+                        Divider()
+                        
+                        HStack {
+                            Text("Fecha limite de inscripción")
+                                .fontWeight(.bold)
+                            
+                            Spacer()
+                            
+                            HStack {
+                                Text(course.startDate!.toISODate(), format: .dateTime.day().month().year())
                             }.foregroundStyle(.secondary)
                             
                         }
@@ -199,6 +209,6 @@ struct CourseDetailView: View {
 
 struct CourseDetailViewPreviews: PreviewProvider{
     static var previews: some View{
-        CourseDetailView(course: Course(id:UUID().uuidString, name: "Curso de Escritura", description: "Lleva tus habilidades para crear artesanias al siguiente nivel, aprende a pintar con acuarelas y tecnicas de dibujo.",speaker: "Tu mama", startDate: Date.now.toString(), endDate: Date.now.toString(), schedule: "16:00", modality: "Presencial", postalCode: 38193, location: "Calle Max Henriquez Ureña #88, apartamento 401jik dxhtdt hdiuhtndgiuhid cshdgifuide uhinntihukgicfic dicg cgdicgidc c cgihd i d d cgicg cgicgi c", status: "De pago", cost: 1200, courseImage: "https://www.grupocibernos.com/hubfs/gestion-de-proyectos-empresariales.jpg", capacity: 1, rating: 0, meetingCode: "hola", accessCode: "ahol", focus: []))
+        CourseDetailView(course: Course(id:UUID().uuidString, name: "Curso de Escritura", description: "Lleva tus habilidades para crear artesanias al siguiente nivel, aprende a pintar con acuarelas y tecnicas de dibujo.",speaker: "Tu mama", startDate: Date.now.toString(), endDate: Date.now.toString(), schedule: "16:00", modality: "Remoto", postalCode: 38193, location: "Calle Max Henriquez Ureña #88, apartamento 401jik dxhtdt hdiuhtndgiuhid cshdgifuide uhinntihukgicfic dicg cgdicgidc c cgihd i d d cgicg cgicgi c", status: "De pago", cost: 1200, courseImage: "https://www.grupocibernos.com/hubfs/gestion-de-proyectos-empresariales.jpg", capacity: 1, rating: 3.4, meetingCode: "hola", accessCode: "ahol", focus: []))
     }
 }
