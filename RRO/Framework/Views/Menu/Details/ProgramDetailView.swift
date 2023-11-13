26058eb7275774274c1fd8aaab4002a64c874a98
karlapa06 <A01705331@tec.mx>
2023-11-13 12:53:40 -0600

    fix: add documentation of programs

--- RRO/Framework/Views/Menu/Details/ProgramDetailView.swift
+++ RRO/Framework/Views/Menu/Details/ProgramDetailView.swift
@@ -6,13 +6,51 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct ProgramDetailView: View {
     var program: Program
     @State var admin = false
     
     var body: some View {
-        Text("Henlo guord")
+        ScrollView{
+            VStack(spacing: 8){
+                if program.programImage != "" {
+                    WebImage(url: URL(string: program.programImage))
+                        .resizable()
+                        .cornerRadius(16)
+                        .scaledToFit()
+                } else {
+                    Image("DefaultImage")
+                        .resizable()
+                        .cornerRadius(16)
+                        .scaledToFit()
+                }
+                HStack{
+                    Text(program.name)
+                        .font(.title)
+                        .fontWeight(.bold)
+                    
+                    Spacer()
+                    
+                    if admin{
+                        Button{
+                        } label:{
+                            Image(systemName: "pencil")
+                            Text("Editar")
+                        }
+                        .fontWeight(.bold)
+                        .padding(8)
+                        .background(Color.red)
+                        .cornerRadius(.infinity)
+                        .foregroundStyle(.white)
+                    }
+                }
+                VStack(spacing:8){
+                    
+                }
+            }
+        }
     }
 }
 
