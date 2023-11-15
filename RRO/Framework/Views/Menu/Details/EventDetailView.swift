3346607201b277646ed3f3fcf2764fb7f65e0695
azul <azul.rosales@outlook.com>
2023-11-14 23:27:06 -0600

    Event detail view

--- RRO/Framework/Views/Menu/Details/EventDetailView.swift
+++ RRO/Framework/Views/Menu/Details/EventDetailView.swift
@@ -6,16 +6,95 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
 
-struct EventDetailView: View {
+struct
+EventDetailView: View {
     var event: Event
+    @State var admin = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            
+            VStack(spacing: 8) {
+                if event.imageUrl != "" {
+                    WebImage(url: URL(string: event.imageUrl))
+                        .resizable()
+                        .cornerRadius(16)
+                        .scaledToFit()
+                } else {
+                    Image("DefaultImage")
+                        .resizable()
+                        .cornerRadius(16)
+                        .scaledToFit()
+                }
+                HStack {
+                    Text(event.eventName)
+                        .font(.title)
+                        .fontWeight(.bold)
+                    
+                    Spacer()
+                    
+                    if admin {
+                        Button {
+                            // TODO: Give the admin access to edit the field
+                        } label: {
+                            Image(systemName: "pencil")
+                            Text("Editar")
+                        }
+                        .fontWeight(.bold)
+                        .padding(8)
+                        .background(Color.red)
+                        .cornerRadius(.infinity)
+                        .foregroundStyle(.white)
+                    }
+                }
+                    
+                VStack(spacing: 8) {
+                    Text("Fecha(s):")
+                    HStack {
+                        Image(systemName: "calendar")
+                        Text(event.startDate?.toISODate() ?? "10/10/10".toISODate(), format: .dateTime.day().month()) + Text(" - ") + Text(event.endDate?.toISODate() ?? "10/10/10".toISODate(), format: .dateTime.day().month())
+                    }
+                }
+                .padding(.vertical)
+                .font(.title3)
+                .fontWeight(.bold)
+                
+                VStack {
+                    
+                    HStack {
+                        Text("Ubicación")
+                            .fontWeight(.bold)
+                        
+                        Spacer()
+                        
+                        Text(event.location)
+                            .foregroundStyle(.secondary)
+                    }
+                    
+                    Divider()
+                                        
+                }.padding(.vertical)
+                
+                VStack(spacing: 8) {
+                    HStack {
+                        Text("Detalles")
+                            .fontWeight(.bold)
+                        Spacer()
+                    }
+                    HStack {
+                        Text(event.description)
+                            .foregroundStyle(.secondary)
+                        
+                        Spacer()
+                    }
+                }
+                
+            }.padding(.horizontal)
+        }
+        .navigationTitle(event.eventName    )
+        .navigationBarTitleDisplayMode(.inline)
     }
-}
-
-struct EventDetailViewPreviews: PreviewProvider {
-    static var previews: some View {
-        EventDetailView(event: Event(id: UUID().uuidString, eventName: "Evento", description: "Descripción del evento", location: "Centro de Congresos", startDate: Date.now.toString(), endDate: Date.now.toString(), imageUrl: ""))
-    }
+        
 }
