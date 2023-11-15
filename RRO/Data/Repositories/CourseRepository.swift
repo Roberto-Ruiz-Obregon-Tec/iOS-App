ca40bddc241da5fbdc3a2c8a2bb9581146faabe3
✳️ Rodrigo Terán <59104775+RodrigoTeran@users.noreply.github.com>
2023-11-14 18:40:46 -0600

    Revert "Feature/0.1.0.beta.rro40 i/consult landing"

--- RRO/Data/Repositories/CourseRepository.swift
+++ RRO/Data/Repositories/CourseRepository.swift
@@ -7,7 +7,6 @@
 
 import Foundation
 
-/// Clase para obtener datos de cursos a través de la API.
 class CourseRepository: CourseAPIProtocol {
     let netService: NetworkAPIService
     static let shared = CourseRepository()
@@ -16,7 +15,6 @@ class CourseRepository: CourseAPIProtocol {
         self.netService = netService // Inicializamos la instancia shared
     }
     
-    /// Función para obtener una lista de curso.
     func getCourseList() async -> ServerResponse<[Course]>?{
         let params = [ // Parametros a agregar en el request
             "cost[gte]": 0
@@ -25,12 +23,6 @@ class CourseRepository: CourseAPIProtocol {
         return await netService.self.get(url: URL(string: "\(API.base)\(API.routes.course)")!, params: params)
     }
     
-    /// Función para obtener información de un curso en específico.
-    ///
-    /// - Parametros:
-    ///   - id: El id del curso.
-    ///
-    /// - Return: La información del curso correspondiente con el id.
     func getCourse(id: String) async -> ServerResponse<[Course]>? {
         return await netService.self.get(url: URL(string: "\(API.base)\(API.routes.course)/\(id)")!)
     }
